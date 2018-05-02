d13473c24a411f24df3aff4f843d7722fcd29c3b
Torrey Smith <torreypsmith@aol.com>
2018-05-02 16:01:38 -0600

    added db connection

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ def setpass():
 @app.route('/dashboard', methods=['GET', 'POST'])
 @is_logged_in
 def dashboard():
-    # if request.method == 'POST':
+    if request.method == 'POST':
     #     # CONFIG TWILIO
     #     account_sid = os.getenv('TWILIO_ACCOUNT_SID')
     #     auth_token = os.getenv('TWILIO_AUTH_TOKEN')
