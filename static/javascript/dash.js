9ed4457afef5e9fe7fdfb205a950d6ca990f8f1b
Torrey Smith <torreypsmith@aol.com>
2018-05-03 13:26:45 -0600

    iphone js

--- static/javascript/dash.js
+++ static/javascript/dash.js
@@ -3,15 +3,20 @@ const clearFields = () => {
   $('#phoneNumberInput').val('')
 }
 
-$('.num').click(function () {
-  let num = $(this)
-  let text = $.trim(num.find('.txt').clone().children().remove().end().text())
-  let telNumber = $('#phoneNumberInput')
-  if ($(telNumber).val().length < 10) {
-    $(telNumber).val(telNumber.val() + text)
-  }
-})
+
+
 
 $(document).ready(() => {
   $('.clear-fields').click(clearFields)
+
+  $('.num').click(function () {
+    let num = $(this)
+    let text = $.trim(num.find('.txt').clone().children().remove().end().text())
+    let telNumber = $('#phoneNumberInput')
+    if ($(telNumber).val().length < 10) {
+      $(telNumber).val(telNumber.val() + text)
+    }
+  })
+
+  
 })
