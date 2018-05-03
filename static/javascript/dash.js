945072957a142168bc899dc7e3511e956462871d
Torrey Smith <torreypsmith@aol.com>
2018-05-03 12:35:56 -0600

    iphone frame and functionallity

--- static/javascript/dash.js
+++ static/javascript/dash.js
@@ -3,7 +3,14 @@ const clearFields = () => {
   $('#phoneNumberInput').val('')
 }
 
-
+$('.num').click(function () {
+  let num = $(this)
+  let text = $.trim(num.find('.txt').clone().children().remove().end().text())
+  let telNumber = $('#phoneNumberInput')
+  if ($(telNumber).val().length < 10) {
+    $(telNumber).val(telNumber.val() + text)
+  }
+})
 
 $(document).ready(() => {
   $('.clear-fields').click(clearFields)
