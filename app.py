37f01b16fd11512e56f90a092ac13397d11ed87e
Torrey Smith <torreypsmith@aol.com>
2018-05-07 11:24:57 -0600

    phone call message

--- app.py
+++ app.py
@@ -270,11 +270,6 @@ def dashboard():
                                 )
         print(call.sid)
 
-        response = VoiceResponse()
-        response.say('Bom dia.', voice='alice', language='pt-BR', loop=2)
-
-        print(response)
-
         return render_template('dashboard.html', form=form)
 
     if request.method == 'POST' and form.sendText.data:
